88ec6781ce42f91641ab6d7c86350cf59e5f7f7b
Tom Sutcliffe <tomsci@me.com>
2014-04-18 15:37:25 +0100

    Rejigged memory map for better feng shui
    (KSuperPageAddress now representable as an assembly immediate, kernel
    stacks no longer right at the bottom so less disruptive to change their
    size in future)

--- k/inc/memmap.h
+++ k/inc/memmap.h
@@ -8,19 +8,19 @@
 /*
 
 Section Zero:
-Guard		-----------------	F8000000-F8001000	(4k)
-Abort stack	00004000-00005000	F8001000-F8002000	(4k)
-Guard		-----------------	F8002000-F8003000	(4k)
-IRQ stack	00005000-00006000	F8003000-F8004000	(4k)
-Guard		-----------------	F8004000-F8005000	(4k)
-Kern stack	00006000-00008000	F8005000-F8007000	(8k)
-Guard		-----------------	F8007000-F8008000	(4k)
+SuperPage						F8000000-F8001000	(4k)
+Sect0 PT	00003000-00004000	F8001000-F8002000	(4k)
+KProcessesSection_pt			F8002000-F8003000	(4k)
+KProcessesPdeSection_pt			F8003000-F8004000	(4k)
+KKernelPdeB	00048000-0004C000	F8004000-F8008000	(16k)
 Code		00008000-00048000	F8008000-F8048000	(256k)
-Kern PDEs	00048000-0004C000	F8048000-F804C000	(16k)
-Sect0 PT	00003000-00004000	F804C000-F804D000	(4k)
-Proc PTEs						F804D000-F804E000	(4k)
-SuperPage						F804E000-F804F000	(4k)
-Process PDEs section PT			F804F000-F8050000	(4k)
+Guard		-----------------	F8048000-F8049000	(4k)
+Abort stack	00004000-00005000	F8049000-F804A000	(4k)
+Guard		-----------------	F804A000-F804B000	(4k)
+IRQ stack	00005000-00006000	F804B000-F804C000	(4k)
+Guard		-----------------	F804C000-F804D000	(4k)
+Kern stack	00006000-00008000	F804D000-F804F000	(8k)
+Guard		-----------------	F804F000-F8050000	(4k)
 KKernPtForProcPts_pt			F8050000-F8051000	(4k)
 Unused		-----------------	F8051000-F80C0000
 PageAlloctr	0004C000-dontcare	F80C0000-F8100000	(256k)
@@ -37,13 +37,13 @@ User PTs						90000000-A0000000	(256MB)
 #define KSectionZero			0xF8000000u
 
 #define KPhysicalSect0Pt		0x00003000u
-#define KSectionZeroPt			0xF804C000u
+#define KSectionZeroPt			0xF8001000u
 
 #define KPhysicalPdeBase		0x00048000u
-#define KKernelPdeBase			0xF8048000u
+#define KKernelPdeBase			0xF8004000u
 
 #define KPhysicalStackBase		0x00006000u
-#define KKernelStackBase		0xF8005000u
+#define KKernelStackBase		0xF804D000u
 #define KKernelStackSize		0x00002000u // 8kB
 
 #define KPhysicalCodeBase		0x00008000u
@@ -53,22 +53,22 @@ User PTs						90000000-A0000000	(256MB)
 #define KPhysicalAbortStackBase	0x00004000u
 #define KPhysicalIrqStackBase	0x00005000u
 
-#define KAbortStackBase			0xF8001000u
-#define KIrqStackBase			0xF8003000u
+#define KAbortStackBase			0xF8049000u
+#define KIrqStackBase			0xF804B000u
 
 #define KPhysPageAllocator		0x0004C000u
 #define KPageAllocatorAddr		0xF80C0000u
 #define KPageAllocatorMaxSize	0x00040000u
 
-#define KSuperPageAddress		0xF804E000u
+#define KSuperPageAddress		0xF8000000u
 
 // Other sections and their page tables
 
 #define KProcessesSection		0xF8100000u
-#define KProcessesSection_pt	0xF804D000u
+#define KProcessesSection_pt	0xF8002000u
 
 #define KProcessesPdeSection	0xF8300000u
-#define KProcessesPdeSection_pt	0xF804F000u
+#define KProcessesPdeSection_pt	0xF8003000u
 
 
 #define KKernPtForProcPts		0xF8200000u
@@ -91,5 +91,10 @@ Thread stacks					0FE00000-10000000
 // Heap assumed to be immediately following BSS in process_init()
 #define KUserHeapBase			0x00008000u
 #define KUserStacksBase			0x0FE00000u
+
+// I'll be generous
+#define USER_STACK_SIZE (16*1024)
+
+#define userStackBase(idx) (KUserStacksBase + idx * (USER_STACK_SIZE + KPageSize))
 
 #endif
