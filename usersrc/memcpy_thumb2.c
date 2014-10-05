54ed682c7ffcfae98e6a133ea726c3ebd9dc76f9
Tom Sutcliffe <tomsci@me.com>
2014-10-05 17:36:01 +0100

    So, I can't write a memcpy function even a dumb one

--- usersrc/memcpy_thumb2.c
+++ usersrc/memcpy_thumb2.c
@@ -10,22 +10,26 @@
 void* memcpy(void* aDest, const void* src, int size) {
 	if (((uintptr)aDest & 3) == 0 && (((uintptr)src) & 3) == 0) {
 		// Simple aligned case
-		const int n = size/4;
+		const int n = size >> 2;
 		const uint32* ptr = (uint32*)src;
 		const uint32* end = ptr + n;
 		uint32* dest = (uint32*)aDest;
 		while (ptr != end) {
 			*dest++ = *ptr++;
 		}
-	} else {
-		// Screw it, be inefficient
-		const int n = size;
-		const uint8* ptr = (uint8*)src;
-		const uint8* end = ptr + n;
-		uint8* dest = (uint8*)aDest;
-		while (ptr != end) {
-			*dest++ = *ptr++;
-		}
+		size = size & 3; // Remainder
+		if (!size) return aDest;
+		src = end;
+		aDest = dest;
+	}
+
+	// Screw it, be inefficient
+	const int n = size;
+	const uint8* ptr = (uint8*)src;
+	const uint8* end = ptr + n;
+	uint8* dest = (uint8*)aDest;
+	while (ptr != end) {
+		*dest++ = *ptr++;
 	}
 	return aDest;
 }
