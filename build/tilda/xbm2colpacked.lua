9ee551e9c78430e44617f218747668b6a94dde3d
Tom Sutcliffe <tomsci@me.com>
2014-11-02 16:28:05 +0000

    Added script to convert XBMs to TiLDA screen native format
    Also added a nice encouraging image to blit in screen_drawCrashed().

---
+++ build/tilda/xbm2colpacked.lua
@@ -0,0 +1,78 @@
+#!/usr/local/bin/lua
+
+--[[**
+Converts an XBM to the column-packed format used by the TiLDA screen
+]]
+
+require "bit32"
+
+name, width, height = nil, nil, nil
+bytes = {}
+packedBytes = {}
+local band, bor, lshift, rshift, bnot = bit32.band, bit32.bor, bit32.lshift, bit32.rshift, bit32.bnot
+
+local function getBit(bitIdx)
+	local byte = bytes[bit32.rshift(bitIdx, 3) + 1]
+	if byte == nil then return nil end
+	return band(byte, lshift(1, band(bitIdx, 7)))
+end
+
+local function byteForPageAndColumn(page, column)
+	local result = 0
+	local x = column
+	local y = page*8
+	for yidx = 0, 7 do
+		local bit = getBit((y + yidx) * stride + x)
+		if bit and bit == 0 then
+			-- bit == 0 means black in XBM terms, meaning we need to set the bit
+			-- to 1 in the format the TiLDA screen uses.
+			result = bor(result, lshift(1, yidx))
+		end
+	end
+	return result
+end
+
+
+for line in io.lines() do
+	local n, w = line:match("#define (.*)_width (.*)")
+	if n then
+		name = n:match("(.*)_xbm") or n
+		width = assert(tonumber(w))
+		stride = bit32.band(width + 7, bnot(7))
+	else
+		local h = line:match("#define .*_height (.*)")
+		if h then height = assert(tonumber(h)) end
+	end
+
+	if line:match("^%s*0x") then
+		for byte in line:gmatch("0x(%x%x)") do
+			table.insert(bytes, tonumber(byte, 16))
+		end
+	end
+end
+
+n = #bytes
+assert(n*8 >= width * height, "Not enough data, need "..tostring(width * height).." bits, only got "..tostring(n*8))
+
+for page = 0, (height + 7) / 8 - 1 do
+	local lineStr = {}
+	for column = 0, width - 1 do
+		table.insert(packedBytes, byteForPageAndColumn(page, column))
+	end
+end
+assert(#packedBytes >= n, "packedBytes shorter than bytes??")
+
+print(string.format("#define %s_width %d", name, width))
+print(string.format("#define %s_npages %d", name, #packedBytes / width))
+print("static const unsigned char "..name.."[] = {")
+for i = 1, #packedBytes, 12 do
+	local line = { "\t" }
+	for j = 0, 11 do
+		local b = packedBytes[i + j]
+		if not b then break end
+		if j > 0 then table.insert(line, " ") end
+		table.insert(line, string.format("0x%02x,", b))
+	end
+	print(table.concat(line))
+end
+print("};")
